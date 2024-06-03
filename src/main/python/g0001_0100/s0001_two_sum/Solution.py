3b62caeb21fb6fb34d2eda76973bb0c2f49000c2
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2024-06-03 22:59:17 +0700

    Added tasks 1-10

---
+++ src/main/python/g0001_0100/s0001_two_sum/Solution.py
@@ -0,0 +1,15 @@
+# #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
+# #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
+# #2024_06_03_Time_47_ms_(97.59%)_Space_17.7_MB_(38.78%)
+
+from typing import List
+
+class Solution:
+        def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        index_map = {}
+        for i, num in enumerate(numbers):
+        required_num = target - num
+        if required_num in index_map:
+        return [index_map[required_num], i]
+        index_map[num] = i
+        return [-1, -1]
