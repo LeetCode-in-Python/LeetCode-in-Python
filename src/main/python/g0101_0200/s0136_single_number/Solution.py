939ae6c9a2e1639cf1ca15d710f4a66521aa872f
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-10 14:42:50 +0300

    Updated readme

--- src/main/python/g0101_0200/s0136_single_number/Solution.py
+++ src/main/python/g0101_0200/s0136_single_number/Solution.py
@@ -1,6 +1,7 @@
 # #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Bit_Manipulation
 # #Data_Structure_II_Day_1_Array #Algorithm_I_Day_14_Bit_Manipulation #Udemy_Integers
-# #Big_O_Time_O(N)_Space_O(1) #2024_06_09_Time_98_ms_(96.80%)_Space_19_MB_(89.40%)
+# #Top_Interview_150_Bit_Manipulation #Big_O_Time_O(N)_Space_O(1)
+# #2024_06_09_Time_98_ms_(96.80%)_Space_19_MB_(89.40%)
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
