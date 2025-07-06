1b37092bd67c8680a2a69a4ceae3f27af8c84a4b
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-06 08:24:30 +0300

    Improved test

--- src/main/python/g0001_0100/s0001_two_sum/Solution_test.py
+++ src/main/python/g0001_0100/s0001_two_sum/Solution_test.py
@@ -2,17 +2,14 @@ import unittest
 from Solution import Solution
 
 class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.sol = Solution()
-
     def test_twoSum(self):
-        self.assertEqual(self.sol.twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(Solution().twoSum([2, 7, 11, 15], 9), [0, 1])
 
     def test_twoSum2(self):
-        self.assertEqual(self.sol.twoSum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(Solution().twoSum([3, 2, 4], 6), [1, 2])
 
     def test_twoSum3(self):
-        self.assertEqual(self.sol.twoSum([3, 3], 6), [0, 1])
+        self.assertEqual(Solution().twoSum([3, 3], 6), [0, 1])
 
     def test_twoSum4(self):
-        self.assertEqual(self.sol.twoSum([3, 3], 7), [-1, -1])
+        self.assertEqual(Solution().twoSum([3, 3], 7), [-1, -1])
