73ebd98d37e1e47061020b04ab8d1e434f8f1bd6
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-06 08:23:18 +0300

    Improved task 1

--- src/main/python/g0001_0100/s0001_two_sum/Solution_test.py
+++ src/main/python/g0001_0100/s0001_two_sum/Solution_test.py
@@ -5,14 +5,14 @@ class TestSolution(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    def test_two_sum(self):
+    def test_twoSum(self):
         self.assertEqual(self.sol.twoSum([2, 7, 11, 15], 9), [0, 1])
 
-    def test_two_sum2(self):
+    def test_twoSum2(self):
         self.assertEqual(self.sol.twoSum([3, 2, 4], 6), [1, 2])
 
-    def test_two_sum3(self):
+    def test_twoSum3(self):
         self.assertEqual(self.sol.twoSum([3, 3], 6), [0, 1])
 
-    def test_two_sum4(self):
+    def test_twoSum4(self):
         self.assertEqual(self.sol.twoSum([3, 3], 7), [-1, -1])
