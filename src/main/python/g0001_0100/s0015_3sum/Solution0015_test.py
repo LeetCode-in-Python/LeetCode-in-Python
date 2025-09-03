4ae851414c0bb1c78eafe50c8382729eedf6b226
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-03 16:41:22 +0300

    Update Solution0015_test.py

--- src/main/python/g0001_0100/s0015_3sum/Solution0015_test.py
+++ src/main/python/g0001_0100/s0015_3sum/Solution0015_test.py
@@ -1,7 +1,6 @@
 import unittest
 from Solution0015 import Solution
 
-
 class SolutionTest(unittest.TestCase):
     def test_threeSum(self):
         self.assertEqual(Solution().threeSum([-1, 0, 1, 2, -1, -4]), [[-1, -1, 2], [-1, 0, 1]])
@@ -10,4 +9,4 @@ class SolutionTest(unittest.TestCase):
         self.assertEqual(Solution().threeSum([]), [])
 
     def test_threeSum3(self):
-        self.assertEqual(Solution().threeSum([0]), []) 
+        self.assertEqual(Solution().threeSum([0]), [])
