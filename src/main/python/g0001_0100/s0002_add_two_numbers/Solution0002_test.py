d2033d314a8b8b07918980c621baa586a23e9837
Valentyn Kolesnikov <javadev75@gmail.com>
2025-11-15 17:50:06 +0200

    Update Solution0002_test.py

--- src/main/python/g0001_0100/s0002_add_two_numbers/Solution0002_test.py
+++ src/main/python/g0001_0100/s0002_add_two_numbers/Solution0002_test.py
@@ -20,19 +20,19 @@ def linked_list_to_str(node):
 
 class SolutionTest(unittest.TestCase):
     def test_addTwoNumbers(self):
-        listNode1 = construct_linked_list([2, 4, 3])
-        listNode2 = construct_linked_list([5, 6, 4])
-        result = Solution().addTwoNumbers(listNode1, listNode2)
+        list_node1 = construct_linked_list([2, 4, 3])
+        list_node2 = construct_linked_list([5, 6, 4])
+        result = Solution().addTwoNumbers(list_node1, list_node2)
         self.assertEqual(linked_list_to_str(result), "7, 0, 8")
 
     def test_addTwoNumbers2(self):
-        listNode1 = ListNode(0)
-        listNode2 = ListNode(0)
-        result = Solution().addTwoNumbers(listNode1, listNode2)
+        list_node1 = ListNode(0)
+        list_node2 = ListNode(0)
+        result = Solution().addTwoNumbers(list_node1, list_node2)
         self.assertEqual(linked_list_to_str(result), "0")
 
     def test_addTwoNumbers3(self):
-        listNode1 = construct_linked_list([9, 9, 9, 9, 9, 9, 9])
-        listNode2 = construct_linked_list([9, 9, 9, 9])
-        result = Solution().addTwoNumbers(listNode1, listNode2)
+        list_node1 = construct_linked_list([9, 9, 9, 9, 9, 9, 9])
+        list_node2 = construct_linked_list([9, 9, 9, 9])
+        result = Solution().addTwoNumbers(list_node1, list_node2)
         self.assertEqual(linked_list_to_str(result), "8, 9, 9, 9, 0, 0, 0, 1")
