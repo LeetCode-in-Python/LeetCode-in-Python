a23d2bb336b9d95de4122a8a430f543a47b90cd8
Thanh829 <93962044+ThanhNIT@users.noreply.github.com>
2026-01-01 07:43:44 +0700

    update

--- src/main/python/g0001_0100/s0009_palindrome_number/Solution0009_test.py
+++ src/main/python/g0001_0100/s0009_palindrome_number/Solution0009_test.py
@@ -3,10 +3,10 @@ from Solution0009 import Solution
 
 class SolutionTest(unittest.TestCase):
     def test_isPalindrome(self):
-        self.assertTrue(Solution().isPalindrome(121), True)
+        self.assertTrue(Solution().isPalindrome(121))
 
     def test_isPalindrome2(self):
-        self.assertFalse(Solution().isPalindrome(-121), False)
+        self.assertFalse(Solution().isPalindrome(-121))
 
     def test_isPalindrome3(self):
-        self.assertFalse(Solution().isPalindrome(10), False)
+        self.assertFalse(Solution().isPalindrome(10))
